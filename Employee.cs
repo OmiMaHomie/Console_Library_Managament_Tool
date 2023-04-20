10324bd1b95f2829297f69de1261d852391ab9d6
Om Khadka <om.khadka.101@icloud.com>
2023-04-20 09:51:06 -0400

    working on making employee class.

---
+++ Employee.cs
@@ -0,0 +1,25 @@
+﻿namespace Console_Library_Management_Tool;
+
+/// <summary>
+/// Represents a high-level employee of the library.
+/// </summary>
+public class Employee : IUser
+{
+    public string Name { get; set; }
+    
+    public string Password { get; set; }
+    
+    public List<Book> CheckedOutBooks { get; init; }
+
+    /// <summary>
+    /// Initializes an employee with a custom name and password, and an empty list of checked out books.
+    /// </summary>
+    /// <param name="name">Desired name of the employee.</param>
+    /// <param name="password">Password the user designated for this profile.</param>
+    public Employee(string name, string password)
+    {
+        Name = name;
+        Password = password;
+        CheckedOutBooks = new();
+    }
+}
